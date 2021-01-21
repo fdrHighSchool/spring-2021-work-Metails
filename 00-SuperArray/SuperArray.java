fe249c45be73764b60e723ff3e07671c464e8843
Metails <dengjianhua2397462613#gmail.com>
2021-01-21 09:17:14 -0500

    Added assignments, Created some methods

---
+++ 00-SuperArray/SuperArray.java
@@ -0,0 +1,17 @@
+public class SuperArray{
+//Instance variables
+private int arraySize;
+
+
+
+
+//----------------------------
+//Constructor method 1
+public SuperArray(int size){
+this.arraySize = size;
+}//end constructor method 1
+//Constructor method 2
+public SuperArray(){
+  this.arraySize = 10;
+}//end constructor method 2
+}//end class
